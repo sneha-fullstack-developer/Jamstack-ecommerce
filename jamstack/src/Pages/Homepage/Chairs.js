998a9ecef7a63d534852a6fef902960d891dbd26
sneha <sneha@c360soft.ai>
2021-08-06 22:28:44 +0530

    Admin Component

--- jamstack/src/Pages/Homepage/Chairs.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Chairs() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
--- jamstack/src/Pages/Homepage/Sofas.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Sofas() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
